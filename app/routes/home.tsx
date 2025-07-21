9248705c35d4416a74ecdf0238924efbc6210fb8
Vikash Thakur <thakvikash622@gmail.com>
2025-07-21 18:48:24 +0530

    updated: init fn and home navigation

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 
 
     useEffect(() => {
-        if(!auth.isAuthenticated) navigate('/auth/next=/')
+        if(!auth.isAuthenticated) navigate('/auth?next=/')
     },[auth.isAuthenticated])
 
 
